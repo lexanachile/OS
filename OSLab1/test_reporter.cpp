e3a8a4f21cf0f947d51a0ab4888de340b99962fe
Aleksey Yarosh <yarashsei@gmail.com>
2025-09-25 08:43:45 +0300

    Починил тест заменив 10.0 на 10.00

--- OSLab1/test_reporter.cpp
+++ OSLab1/test_reporter.cpp
@@ -5,10 +5,47 @@
 
 using namespace std;
 
+bool generate_report_for_test(const string& bin_filename, const string& report_filename, double hourly_rate)
+{
+    ifstream file_in(bin_filename, ios::binary);
+    if (!file_in.is_open())
+    {
+        cerr << "Error: Cannot open binary file " << bin_filename << endl;
+        return false;
+    }
+
+    ofstream report_out(report_filename);
+    if (!report_out.is_open())
+    {
+        cerr << "Error: Cannot create report file " << report_filename << endl;
+        file_in.close();
+        return false;
+    }
+
+    report_out << "Report for file \"" << bin_filename << "\"\n";
+    report_out << "Employee number, employee name, hours, salary\n";
+
+    employee emp;
+    while (file_in.read(reinterpret_cast<char*>(&emp), sizeof(employee)))
+    {
+        double salary = emp.hours * hourly_rate;
+        report_out << emp.num << ", "
+                   << emp.name << ", "
+                   << fixed << setprecision(2) << emp.hours << ", "
+                   << fixed << setprecision(2) << salary << "\n";
+    }
+
+    file_in.close();
+    report_out.close();
+    return true;
+}
+
+
 TEST(ReporterTest, GenerateReportSuccess)
 {
     string bin_file = "test_bin.bin";
     string report_file = "test_report.txt";
+    double hourly_rate = 150.0;
 
     employee test_employees[] = {
         {1, "Ivanov", 10.0},
@@ -22,24 +59,7 @@ TEST(ReporterTest, GenerateReportSuccess)
     }
     bin_out.close();
 
-    ifstream file_in(bin_file, ios::binary);
-    ofstream report_out(report_file);
-
-    report_out << "Report for file \"" << bin_file << "\"\n";
-    report_out << "Employee number, employee name, hours, salary\n";
-
-    employee emp;
-    while (file_in.read(reinterpret_cast<char*>(&emp), sizeof(employee)))
-    {
-        double salary = emp.hours * 150.0;
-        report_out << emp.num << ", "
-                   << emp.name << ", "
-                   << emp.hours << ", "
-                   << fixed << setprecision(2) << salary << "\n";
-    }
-
-    file_in.close();
-    report_out.close();
+    generate_report_for_test(bin_file, report_file, hourly_rate);
 
     ifstream report_in(report_file);
     ASSERT_TRUE(report_in.is_open());
